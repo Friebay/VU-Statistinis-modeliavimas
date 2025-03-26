e5bf6463735ded9eb0d1f76f391daa54f703dd36
Friebay <frybaylt@gmail.com>
2025-03-26 09:23:48 +0200

    Create MK_Uzduotis_1_dalis.py

---
+++ MK_Uzduotis_1_dalis.py
@@ -0,0 +1,7 @@
+'''
+MK užduotis 1-5
+1. Sugeneruokite pseudoatsitiktiniu skaičių sekas tiesiniu kongruentiniu
+metodu su maksimaliu periodu, kai modulis m = 776 ir m = 1107.
+Daugiklius a parinkite taip, kad galingumai butu didžiausi. Prieauglio c parinkimui
+naudokites gretimu nariu koreliacija (teoriniai testai).
+'''
